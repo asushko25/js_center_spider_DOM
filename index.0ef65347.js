869d808e5351fe083596274113a9dd2ab5785ae9
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-07-10 16:43:11 +0200

    make build

---
+++ index.0ef65347.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");t.style.position="absolute",t.style.top="50%",t.style.left="50%",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.0ef65347.js.map
